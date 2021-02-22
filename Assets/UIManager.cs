2c876d58a21baefed95271eda704a0f08a645304
NYUAD <nyuad@C3-019-IMACP-14.local>
2021-02-22 23:59:53 +0400

    Final UI flow, movement scripts, audio

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -4,27 +4,34 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+	// Audio player game objects
     public AudioSource manilaMusic;
+    public AudioSource jeepAudio;
     public AudioSource barkerAudio;
+    public AudioSource bayadAudio;
 
+    // UI state game objects
     public GameObject titleText;
     public GameObject introScreen;
     public GameObject daddyScreen;
     public GameObject barkerScreen;
     public GameObject bayadScreen;
+    public GameObject endScreen;
 
+    // Object for jeepney model
     public GameObject jeepneyModel;
 
-    int uiState;
+    // UI state tracker (essentially start screen vs. inner UI screens)
+    int uiState = 0;
+
+    // Rotate the jeepney??
     bool rotateOn = true;
 
-    // Start is called before the first frame update
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
         switch(uiState)
@@ -38,6 +45,8 @@ public class UIManager : MonoBehaviour
         RotateJeepney();
     }
 
+
+    // Bunch of UI state switch functions
     void ShowIntroText(){
     	if(!manilaMusic.isPlaying)
     	{
@@ -48,28 +57,111 @@ public class UIManager : MonoBehaviour
     }
 
     public void GetStarted(){
-    	// Hide and show new UI elems
     	introScreen.SetActive(false);
     	daddyScreen.SetActive(true);
     	rotateOn = false;
+    	StartCoroutine(MoveJeepneySide());
+    	jeepAudio.Play();
     }
 
    	public void DaddyPress(){
    		daddyScreen.SetActive(false);
    		barkerScreen.SetActive(true);
+   		jeepAudio.Stop();
    		barkerAudio.Play();
    	}
 
    	public void BarkerPress(){
-   		barkerAudio.Stop();
    		barkerScreen.SetActive(false);
    		bayadScreen.SetActive(true);
+   		barkerAudio.Stop();
+   		bayadAudio.Play();
    	}
 
+   	public void BayadPress(){
+   		bayadScreen.SetActive(false);
+   		endScreen.SetActive(true);
+   		bayadAudio.Stop();
+   		rotateOn = true;
+   		StartCoroutine(MoveJeepneyCenter());
+   	}
+
+   	public void RestartPress(){
+   		endScreen.SetActive(false);
+   		introScreen.SetActive(true);
+   	}
+
+   	// Rotate jeepney during start screen
     void RotateJeepney(){
     	if(rotateOn){
 	    	jeepneyModel.transform.Rotate(0f,0.1f,0f);
     	}    
+    }
+
+    // The IEnumerator function type allows
+    // us to run the movement script as a coroutine.
+    public IEnumerator MoveJeepneySide(){
+    	// Set total time allowed for movement, + initialize current time spent moving
+    	float totalMovementTime = 2f;
+	    float currentMovementTime = 0f;
+
+	    // Starting position
+	    // We need a starting position because we compute a linear
+	    // interpolation between start and end Vector3s.
+	    // The rotation doesn't need one because Quaternion.RotateTowards
+	    // works with the object's current rotation anyway.
+	    Vector3 startingPosition = jeepneyModel.transform.position;
+	    Debug.Log(startingPosition);
+
+	    // Ending position and rotation
+	    Vector3 destinationPosition = new Vector3(10f,-5.36f, 33.8f);
+	    Quaternion destinationRotation = Quaternion.Euler(new Vector3(0f,-126.632f,0f));
+
+	    // Move until in position
+	    while (Vector3.Distance(jeepneyModel.transform.position, destinationPosition) > 0f) {
+	        currentMovementTime += Time.deltaTime;
+
+	        // Lerping interpolates between start and end position based on
+	        // a sort of completion percentage (e.g. time elapsed over the 
+	        // length we want the movement to be)
+	        jeepneyModel.transform.position = Vector3.Lerp(startingPosition, 
+	        	destinationPosition, 
+	        	currentMovementTime / totalMovementTime);
+
+	        // Nice fancy rotate function for EZ and clean rotation calculation
+	        jeepneyModel.transform.rotation = Quaternion.RotateTowards(jeepneyModel.transform.rotation, 
+	        	destinationRotation,
+	        	currentMovementTime / totalMovementTime);
+	        yield return null;
+	    }
+    }
+
+    public IEnumerator MoveJeepneyCenter(){
+    	// Set total time allowed for movement, + initialize current time spent moving
+    	float totalMovementTime = 2f;
+	    float currentMovementTime = 0f;
+
+	    // Starting position
+	    Vector3 startingPosition = jeepneyModel.transform.position;
+
+	    // Ending position
+	    Vector3 destinationPosition = new Vector3(0.8f,-1.4f,27.2f);
+
+	    // Move until in position
+	    // This time, we only move the jeepney without rotating it
+	    // since it'll be rotating constantly anyway
+	    while (Vector3.Distance(jeepneyModel.transform.position, destinationPosition) > 0f) {
+	        currentMovementTime += Time.deltaTime;
+
+	        // Lerping interpolates between start and end position based on
+	        // a sort of completion percentage (e.g. time elapsed over the 
+	        // length we want the movement to be)
+	        jeepneyModel.transform.position = Vector3.Lerp(startingPosition, 
+	        	destinationPosition, 
+	        	currentMovementTime / totalMovementTime);
+
+	        yield return null;
+	    }
     }
 
 }
